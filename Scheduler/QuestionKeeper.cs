07736b727efa68d3ea8d5c258902a8a8aaa33c52
Ryan Kubow <ryankubow@gmail.com>
2021-09-13 17:02:52 -0700

    Fixed question formatting  so that the example prompt was correct.

--- Scheduler/QuestionKeeper.cs
+++ Scheduler/QuestionKeeper.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Scheduler
-{
+{//--FEATURE IDEA-- Find way to have user create schedule times, then ask questions in relation? Would be much more efficient and is entirely possible.
     class QuestionKeeper
     {
         public List<QuestionCouple> QuestionCouples { get; private set; }
@@ -15,20 +15,20 @@ namespace Scheduler
             QuestionCouples = new List<QuestionCouple>
             {
                 new QuestionCouple
-                (new Question(false, "Please enter when you go to bed (hh/mm, military)"),
-                new Question(true, "Please enter how long you sleep (hh/mm, military)")),
+                (new Question(false, "Please enter when you go to bed (ex: hh/mm AM/PM, Military also accepted.)"),
+                new Question(true, "Please enter how long you sleep (ex: 3h 30m = 3 hours and 30 mins)")),
 
                 new QuestionCouple
-                (new Question(false, "Please enter the time of day you eat breakfast (hh/mm, military)"),
-                new Question(true, "Please enter how long you eat breakfast (hh/mm, military)")),
+                (new Question(false, "Please enter the time of day you eat breakfast (ex: hh/mm AM/PM, Military also accepted.)"),
+                new Question(true, "Please enter how long you eat breakfast (ex: 3h 30m = 3 hours and 30 mins)")),
 
                 new QuestionCouple
-                (new Question(false, "Please enter the time of day you eat lunch (hh/mm, military)"),
-                new Question(true, "Please enter how long you eat lunch (hh/mm, military)")),
+                (new Question(false, "Please enter the time of day you eat lunch (ex: hh/mm AM/PM, Military also accepted.)"),
+                new Question(true, "Please enter how long you eat lunch (ex: 3h 30m = 3 hours and 30 mins)")),
 
                 new QuestionCouple
-                (new Question(false, "Please enter the time of day you eat dinner (hh/mm, military)"),
-                new Question(true, "Please enter how long you eat dinner (hh/mm, military)"))
+                (new Question(false, "Please enter the time of day you eat dinner (ex: hh/mm AM/PM, Military also accepted.)"),
+                new Question(true, "Please enter how long you eat dinner (ex: 3h 30m = 3 hours and 30 mins)"))
             };
         }
         public void Ask(int index, int question)
